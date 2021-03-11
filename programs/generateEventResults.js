b570b542773fb8931eb4e4202bdd0e82edf1400b
Samu <samu.linden@gmail.com>
2021-03-11 13:30:20 +0100

    Adds replaces undefined races and qualis with NA

--- programs/generateEventResults.js
+++ programs/generateEventResults.js
@@ -57,10 +57,12 @@ const generateRaceResults = async (resultFile, eventResultFile) => {
           key = driver[0];
         }
 
+        resObj[raceNum][key].raceNum = raceNum + 1;
+
         if (driver.length === 1) {
           // quali
           // driver is in ["name"] format
-          resObj[raceNum][driver[0]].quali = i + 1;
+          resObj[raceNum][key].quali = i + 1;
         } else if (driver.length === 2) {
           // race
           // driver is in ["name", "points"] format
@@ -68,7 +70,7 @@ const generateRaceResults = async (resultFile, eventResultFile) => {
           resObj[raceNum][key].points = driver[1];
         } else {
           throw new Error(
-            `Error in comining quali and race results. Incorrect driver.length: ${driver.length}`
+            `Error in combining quali and race results. Incorrect driver.length: ${driver.length}`
           );
         }
       });
@@ -99,28 +101,83 @@ const generateRaceResults = async (resultFile, eventResultFile) => {
 
   // Count total points and sort
   const finalResultArr = Object.entries(final)
-    .map((res) => {
-      const driverPoints = res[1].reduce((acc, cur) => {
-        acc += cur.points;
+    .map(([name, races]) => {
+      const totalPoints = races.reduce((acc, cur) => {
+        // If player is not present in the final screenshot give 0 points
+        acc += cur.points ? cur.points : 0;
         return acc;
       }, 0);
-      return [res[0], ...res[1], driverPoints];
+      return { name, races, totalPoints };
     })
-    .sort((a, b) => b[4] - a[4]);
+    .sort((a, b) => b.totalPoints - a.totalPoints);
+
+  // Prepare result header row (most likely to have driven all qualis and races)
+  const winner = finalResultArr[0];
+  const hasQuali = !!winner.races[0].quali;
+  const numOfRaces = winner.races[winner.races.length - 1].raceNum;
+  const raceHeaders = new Array(winner.races.length)
+    .fill("")
+    .map((_, i) => {
+      return hasQuali ? `q${i + 1}\tr${i + 1}` : `r${i + 1}`;
+    })
+    .join("\t");
+  const headerString = `name\t${raceHeaders}\tpoints\n`;
+
+  // write headers
+  fs.appendFileSync(eventResultFile, headerString, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
 
   // Prepare driver string and write them in the result file
   finalResultArr.forEach((driver) => {
-    const name = driver.splice(0, 1)[0];
-    const points = driver.splice(driver.length - 1)[0];
-    const results = driver
+    let raceCounter = 1;
+
+    // Create result strings
+    const results = driver.races
       .map((race) => {
-        return race.quali ? `${race.quali}\t${race.race}` : race.race;
+        // Check if a driver has missed both quali and race of a race and
+        // fill with NA
+        let missedRaceStr = "";
+        if (raceCounter < race.raceNum) {
+          const numOfMissedRaces = race.raceNum - raceCounter;
+          const naStr = hasQuali ? "NA\tNA\t" : "NA\t";
+          missedRaceStr = naStr.repeat(numOfMissedRaces);
+          raceCounter = race.raceNum;
+        }
+        raceCounter++;
+
+        // Check if a driver has missed quali OR race and fill with NA
+        const racePos = race.race ? race.race : "NA";
+        const resStr = hasQuali
+          ? race.quali
+            ? `${race.quali}\t${racePos}`
+            : `NA\t${racePos}`
+          : racePos;
+        return `${missedRaceStr}${resStr}`;
       })
       .join("\t");
 
+    // Add fill rows with NA if races missing from the end of the event
+    // This is useful if somebody quits and doesnt drive all races.
+    const finalResults = results.split("\n").map((r) => {
+      const fullResultLength = hasQuali ? numOfRaces * 2 : numOfRaces;
+      if (r.split("\t").length < fullResultLength) {
+        const arr = r.split("\t");
+        while (arr.length < fullResultLength) {
+          arr.push("NA");
+        }
+        return arr.join("\t");
+      }
+
+      return r;
+    });
+
+    // write result rows
     fs.appendFileSync(
       eventResultFile,
-      `${name}\t${results}\t${points}\n`,
+      `${driver.name}\t${finalResults}\t${driver.totalPoints}\n`,
       (err) => {
         if (err) {
           return console.log(err);
